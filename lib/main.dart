6a2dcc356127f632c4812852c9caa18746d2015f
jignasa solanki <jignasasolanki95@gmail.com>
2026-06-26 14:45:45 +0530

    refactor: neccessary change for  go_router in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:crypto_bepay/features/wallet/presentation/screens/wallet_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
